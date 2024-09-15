e093475b6830b7aa6f1077ae1b9a26ffc461ae38
2894462606 <2894462606@qq.com>
2024-09-15 22:29:45 +0800

    feat(generator): 添加 Controller 生成模板
    添加 Controller 生成模板

--- cutepikachu-server-generator/src/main/java/cn/cutepikachu/generator/MyBatisPlusGenerator.java
+++ cutepikachu-server-generator/src/main/java/cn/cutepikachu/generator/MyBatisPlusGenerator.java
@@ -26,8 +26,7 @@ import java.util.Map;
  */
 public class MyBatisPlusGenerator {
 
-    private static final String OUTPUT_DIR =
-            System.getProperty("user.dir") + "/cutepikachu-server-generator/src/main/java";
+    private static final String OUTPUT_DIR = System.getProperty("user.dir") + "/cutepikachu-server-generator/src/main/java";
 
     private static final String AUTH = "笨蛋皮卡丘";
 
@@ -44,125 +43,86 @@ public class MyBatisPlusGenerator {
     private static final String PASSWORD = "root";
 
     public static void main(String[] args) {
-        String url =
-                String.format(
-                        "jdbc:mysql://localhost:3306/%s?remarks=true&useInformationSchema=true&tinyInt1isBit=true",
-                        DATABASE);
+        String url = String.format("jdbc:mysql://localhost:3306/%s?remarks=true&useInformationSchema=true&tinyInt1isBit=true", DATABASE);
 
-        DataSourceConfig.Builder dataSourceConfig =
-                new DataSourceConfig.Builder(url, USERNAME, PASSWORD);
+        DataSourceConfig.Builder dataSourceConfig = new DataSourceConfig.Builder(url, USERNAME, PASSWORD);
 
         FastAutoGenerator
                 // 数据源配置
                 .create(dataSourceConfig)
                 // 全局配置
-                .globalConfig(globalConfigBuilder ->
-                        globalConfigBuilder
-                                .outputDir(OUTPUT_DIR) // 输出目录
-                                .disableOpenDir() // 禁止打开输出目录
-                                .author(AUTH) // 作者名
-                                .dateType(DateType.TIME_PACK) // 时间策略
-                                .commentDate("yyyy-MM-dd HH:mm:ss") // 注释日期时间格式
-                )
-                .packageConfig(packageConfigBuilder ->
-                        packageConfigBuilder
-                                .parent(OUTPUT_PACKAGE) // 父包名
-                                // .moduleName("application") // 父包模块名
-                                .entity("model.entity") // Entity 包名
-                                .service("service") // Service 包名
-                                .serviceImpl("service.impl") // Service Impl 包名
-                                .mapper("mapper") // Mapper 包名
-                                .xml("mapper.xml") // Mapper XML 包名
-                                .controller("controller") // Controller 包名
+                .globalConfig(globalConfigBuilder -> globalConfigBuilder.outputDir(OUTPUT_DIR) // 输出目录
+                        .disableOpenDir() // 禁止打开输出目录
+                        .author(AUTH) // 作者名
+                        .dateType(DateType.TIME_PACK) // 时间策略
+                        .commentDate("yyyy-MM-dd HH:mm:ss") // 注释日期时间格式
+                ).packageConfig(packageConfigBuilder -> packageConfigBuilder.parent(OUTPUT_PACKAGE) // 父包名
+                        // .moduleName("application") // 父包模块名
+                        .entity("model.entity") // Entity 包名
+                        .service("service") // Service 包名
+                        .serviceImpl("service.impl") // Service Impl 包名
+                        .mapper("mapper") // Mapper 包名
+                        .xml("mapper.xml") // Mapper XML 包名
+                        .controller("controller") // Controller 包名
                 )
                 // 策略配置
-                .strategyConfig(strategyConfigBuilder ->
-                        strategyConfigBuilder
-                                .enableCapitalMode()
-                                .enableSkipView()
+                .strategyConfig(strategyConfigBuilder -> strategyConfigBuilder.enableCapitalMode().enableSkipView()
 
-                                // 实体策略配置
-                                .entityBuilder()
-                                .enableColumnConstant() // 开启生成字段常量
-                                .enableChainModel() // 开启链式模型
-                                .enableLombok() // 开启 lombok 模型
-                                .enableTableFieldAnnotation() // 开启生成实体时生成字段注解
-                                .enableActiveRecord() // 开启 ActiveRecord 模型
-                                .enableRemoveIsPrefix() // 开启移除 is 前缀
-                                .superClass(BaseEntity.class) // 继承的 Entity 类
-                                .logicDeleteColumnName("is_delete") // 逻辑删除字段名(数据库字段)
-                                .naming(NamingStrategy.underline_to_camel) // 数据库表映射到实体的命名策略
-                                .columnNaming(NamingStrategy.underline_to_camel) // 数据库表字段映射到实体的命名策略
-                                .addIgnoreColumns("create_time", "update_time", "is_delete") // 忽略字段
-                                .addTableFills(
-                                        new Column("create_time", FieldFill.INSERT),
-                                        new Column("update_time", FieldFill.INSERT_UPDATE)) // 添加表字段填充
-                                .idType(IdType.ASSIGN_ID) // 全局主键类型
-                                .enableFileOverride() // 覆盖文件
-                                .javaTemplate("/templates/model/entity/entity.java") // 模板
+                        // 实体策略配置
+                        .entityBuilder().enableColumnConstant() // 开启生成字段常量
+                        .enableChainModel() // 开启链式模型
+                        .enableLombok() // 开启 lombok 模型
+                        .enableTableFieldAnnotation() // 开启生成实体时生成字段注解
+                        .enableActiveRecord() // 开启 ActiveRecord 模型
+                        .enableRemoveIsPrefix() // 开启移除 is 前缀
+                        .superClass(BaseEntity.class) // 继承的 Entity 类
+                        .logicDeleteColumnName("is_delete") // 逻辑删除字段名(数据库字段)
+                        .naming(NamingStrategy.underline_to_camel) // 数据库表映射到实体的命名策略
+                        .columnNaming(NamingStrategy.underline_to_camel) // 数据库表字段映射到实体的命名策略
+                        .addIgnoreColumns("create_time", "update_time", "is_delete") // 忽略字段
+                        .addTableFills(new Column("create_time", FieldFill.INSERT), new Column("update_time", FieldFill.INSERT_UPDATE)) // 添加表字段填充
+                        .idType(IdType.ASSIGN_ID) // 全局主键类型
+                        .enableFileOverride() // 覆盖文件
+                        .javaTemplate("/templates/model/entity/entity.java") // 模板
 
-                                // controller 策略配置
-                                .controllerBuilder()
-                                .enableRestStyle() // 开启生成 @RestController 控制器
-                                .enableFileOverride() // 覆盖文件
-                                .enableHyphenStyle() // 开启驼峰转连字符
-                                .template("/templates/controller/controller.java") // 模板
-                                .disable()
+                        // controller 策略配置
+                        .controllerBuilder().enableRestStyle() // 开启生成 @RestController 控制器
+                        .enableFileOverride() // 覆盖文件
+                        .enableHyphenStyle() // 开启驼峰转连字符
+                        .template("/templates/controller/controller.java") // 模板
+                        .disable()
 
-                                // mapper 策略配置
-                                .mapperBuilder()
-                                .mapperAnnotation(Mapper.class) // 开启 @Mapper 注解
-                                .enableBaseResultMap() // 启用 BaseResultMap 生成
-                                .enableBaseColumnList() // 启用 BaseColumnList
-                                .enableFileOverride() // 覆盖文件
-                                .mapperTemplate("/templates/mapper/mapper.java") // 模板
-                                .mapperXmlTemplate("/templates/mapper/xml/mapper.xml") // XML 模板
+                        // mapper 策略配置
+                        .mapperBuilder().mapperAnnotation(Mapper.class) // 开启 @Mapper 注解
+                        .enableBaseResultMap() // 启用 BaseResultMap 生成
+                        .enableBaseColumnList() // 启用 BaseColumnList
+                        .enableFileOverride() // 覆盖文件
+                        .mapperTemplate("/templates/mapper/mapper.java") // 模板
+                        .mapperXmlTemplate("/templates/mapper/xml/mapper.xml") // XML 模板
 
-                                // service 策略配置
-                                .serviceBuilder()
-                                .superServiceClass(IService.class) // Service 接口父类
-                                .superServiceImplClass(ServiceImpl.class) // Service 实现类父类
-                                .enableFileOverride() // 覆盖文件
-                                .serviceTemplate("/templates/service/service.java") // 模板
-                                .serviceImplTemplate("/templates/service/impl/serviceImpl.java") // 模板
+                        // service 策略配置
+                        .serviceBuilder().superServiceClass(IService.class) // Service 接口父类
+                        .superServiceImplClass(ServiceImpl.class) // Service 实现类父类
+                        .enableFileOverride() // 覆盖文件
+                        .serviceTemplate("/templates/service/service.java") // 模板
+                        .serviceImplTemplate("/templates/service/impl/serviceImpl.java") // 模板
                 )
                 // 模板引擎
                 .templateEngine(new FreemarkerTemplateEngine())
                 // 注入配置
                 .injectionConfig(injectionConfigBuilder -> {
                     // VO 模板
-                    CustomFile voFile =
-                            new CustomFile.Builder()
-                                    .fileName("VO.java")
-                                    .packageName("model.vo")
-                                    .templatePath("/templates/model/vo/vo.java.ftl")
-                                    .enableFileOverride()
-                                    .build();
+                    CustomFile voFile = new CustomFile.Builder().fileName("VO.java").packageName("model.vo").templatePath("/templates/model/vo/vo.java.ftl").enableFileOverride().build();
+                    // Controller 模板
+                    CustomFile controllerFile = new CustomFile.Builder().fileName("Controller.java").packageName("controller").templatePath("/templates/controller/controller.java.ftl").enableFileOverride().build();
                     // QueryDTO 模板
-                    CustomFile queryRequestFile =
-                            new CustomFile.Builder()
-                                    .fileName("QueryDTO.java")
-                                    .packageName("model.dto")
-                                    .templatePath("/templates/model/dto/queryDTO.java.ftl")
-                                    .enableFileOverride()
-                                    .build();
+                    CustomFile queryRequestFile = new CustomFile.Builder().fileName("QueryDTO.java").packageName("model.dto").templatePath("/templates/model/dto/queryDTO.java.ftl").enableFileOverride().build();
                     // CreateDTO 模板
-                    CustomFile createRequest =
-                            new CustomFile.Builder()
-                                    .fileName("CreateDTO.java")
-                                    .packageName("model.dto")
-                                    .templatePath("/templates/model/dto/createDTO.java.ftl")
-                                    .enableFileOverride()
-                                    .build();
+                    CustomFile createRequest = new CustomFile.Builder().fileName("CreateDTO.java").packageName("model.dto").templatePath("/templates/model/dto/createDTO.java.ftl").enableFileOverride().build();
                     // UpdateDTO 模板
-                    CustomFile updateRequest =
-                            new CustomFile.Builder()
-                                    .fileName("UpdateDTO.java")
-                                    .packageName("model.dto")
-                                    .templatePath("/templates/model/dto/updateDTO.java.ftl")
-                                    .enableFileOverride()
-                                    .build();
+                    CustomFile updateRequest = new CustomFile.Builder().fileName("UpdateDTO.java").packageName("model.dto").templatePath("/templates/model/dto/updateDTO.java.ftl").enableFileOverride().build();
                     injectionConfigBuilder.customFile(voFile);
+                    injectionConfigBuilder.customFile(controllerFile);
                     injectionConfigBuilder.customFile(queryRequestFile);
                     injectionConfigBuilder.customFile(createRequest);
                     injectionConfigBuilder.customFile(updateRequest);
@@ -174,13 +134,13 @@ public class MyBatisPlusGenerator {
                     injectionConfigBuilder.customMap(customMap);
 
                     // 自定义输出文件
-                    injectionConfigBuilder.beforeOutputFile(
-                            (tableInfo, objectMap) -> {
-                                String entity = objectMap.get("entity").toString();
-                                String vo = entity + "VO";
-                                objectMap.put("vo", vo);
-                            });
-                })
-                .execute();
+                    injectionConfigBuilder.beforeOutputFile((tableInfo, objectMap) -> {
+                        String entity = objectMap.get("entity").toString();
+                        String vo = entity + "VO";
+                        objectMap.put("vo", vo);
+                        String service = entity.substring(0, 1).toLowerCase() + entity.substring(1) + "Service";
+                        objectMap.put("service", service);
+                    });
+                }).execute();
     }
 }
---
+++ cutepikachu-server-generator/src/main/resources/templates/controller/controller.java.ftl
@@ -0,0 +1,35 @@
+package ${package.Controller};
+
+<#if restControllerStyle>
+import org.springframework.web.bind.annotation.RestController;
+<#else>
+import org.springframework.stereotype.Controller;
+</#if>
+<#if superControllerClassPackage??>
+import ${superControllerClassPackage};
+</#if>
+import ${package.Service}.${table.serviceName};
+import jakarta.annotation.Resource;
+
+/**
+ * 对外接口
+ *
+ * @author <a href="https://github.com/cutepikachu-cn">笨蛋皮卡丘</a>
+ * @version ${version}
+ * @since ${date}
+ */
+<#if restControllerStyle>
+@RestController
+<#else>
+@Controller
+</#if>
+<#if superControllerClass??>
+public class ${table.controllerName} extends ${superControllerClass} {
+<#else>
+public class ${table.controllerName} {
+</#if>
+
+    @Resource
+    private ${table.serviceName} ${service};
+
+}
